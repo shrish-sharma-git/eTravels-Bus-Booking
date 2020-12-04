e2ed1a12c0ea6759e5440b51a53859b8d0e5c1e4
Shrish Sharma <shrish0608@gmail.com>
2020-12-04 17:57:13 +0530

    Landing Page Completed
    Added Add Bus Button - CUSTOM
    Added Search Bus Button - CUSTOM

--- landing.py
+++ landing.py
@@ -13,8 +13,12 @@ img.place(x=0, y=0)
 
 
 # Buttons
-add_bus = Button(root, text="Add Bus").pack()
-search_bus = Button(root, text="Search Bus").pack()
+add_bus_img = PhotoImage(file = './Assets/add_bus.png')
+add_bus = Button(root, image=add_bus_img, bd=0, bg="#ffd5bc", activebackground="#ffd5bc")
+add_bus.place(x=20, y=400)
 
+search_bus_img = PhotoImage(file = './Assets/search_bus.png')
+search_bus = Button(root, image=search_bus_img, bd=0, bg="#ffd5bc", activebackground="#ffd5bc")
+search_bus.place(x=470, y=400)
 
 root.mainloop()
