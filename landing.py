6162318202711248dc05afaf078cb7a95e676487
Shrish Sharma <shrish0608@gmail.com>
2020-12-06 12:51:01 +0530

    Added "Add Bus" Operator Form
    + a Add to DB Button (Beta)

--- landing.py
+++ landing.py
@@ -24,20 +24,109 @@ render = ImageTk.PhotoImage(load)
 img = Label(root, image=render)
 img.place(x=0, y=0)
 
+#********************* ADD BUS WINDOW **********************
 # Add Bus Window
 def AddBus():
     add_bus_app = Tk()
     add_bus_app.geometry('700x500+120+120')
-    labelExample = Label(add_bus_app, text = "Welcome Operator!").pack()
-    home_btn = Button(add_bus_app, text="Home", command=add_bus_app.destroy).pack()
-    
-        
+
+    labelExample = Label(add_bus_app, text = "Welcome Operator!").grid(row=0, column=1, columnspan=10)
+    #home_btn = Button(add_bus_app, text="Home", command=add_bus_app.destroy).pack()
+
+    # Header
+    head_label = Label(add_bus_app, text="Add Details of Buses").grid(row=1, column=1, pady=10, columnspan=10)
+
+    #Getting Inputs    
+    Agency_name = Entry(add_bus_app, width=60)
+    Agency_name.grid(row=2, column=1, padx=20)
+
+    Contact_Number = Entry(add_bus_app, width=60)
+    Contact_Number.grid(row=3, column=1)
+
+    Address = Entry(add_bus_app, width=60)
+    Address.grid(row=4, column=1)
+
+    Operator_Id = Entry(add_bus_app, width=60)
+    Operator_Id.grid(row=5, column=1)
+
+    Bus_Type = Entry(add_bus_app, width=60)
+    Bus_Type.grid(row=6, column=1)
+
+    loc_From = Entry(add_bus_app, width=60)
+    loc_From.grid(row=7, column=1)
+
+    loc_To = Entry(add_bus_app, width=60)
+    loc_To.grid(row=8, column=1)
+
+    Date = Entry(add_bus_app, width=60)
+    Date.grid(row=9, column=1)
+
+    Dep_Time = Entry(add_bus_app, width=60)
+    Dep_Time.grid(row=10, column=1)
+
+    Arrival_Time = Entry(add_bus_app, width=60)
+    Arrival_Time.grid(row=11, column=1)
+
+    Fare = Entry(add_bus_app, width=60)
+    Fare.grid(row=12, column=1)
+
+    Seats = Entry(add_bus_app, width=60)
+    Seats.grid(row=13, column=1)
+
+    # Adding Entry Labels
+    Agency_name_label = Label(add_bus_app, text="Agency Name")
+    Agency_name_label.grid(row=2, column=0)
+
+    Contact_Number_label = Label(add_bus_app, text="Contact Number")
+    Contact_Number_label.grid(row=3, column=0)
+
+    Address_label = Label(add_bus_app, text="Address")
+    Address_label.grid(row=4, column=0)
+
+    Operator_Id_label = Label(add_bus_app, text="Operator Id")
+    Operator_Id_label.grid(row=5, column=0)
+
+    Bus_Type_label = Label(add_bus_app, text="Bus Type")
+    Bus_Type_label.grid(row=6, column=0)
+
+    loc_From_label = Label(add_bus_app, text="From Location")
+    loc_From_label.grid(row=7, column=0)
+
+    loc_To_label = Label(add_bus_app, text="To Loaction")
+    loc_To_label.grid(row=8, column=0)
+
+    Date_label = Label(add_bus_app, text="Date")
+    Date_label.grid(row=9, column=0)
+
+    Dep_Time_label = Label(add_bus_app, text="Departure Time")
+    Dep_Time_label.grid(row=10, column=0)
+
+    Arrival_Time_label = Label(add_bus_app, text="Arrival TIme")
+    Arrival_Time_label.grid(row=11, column=0)
+
+    Fare_label = Label(add_bus_app, text="$ Fare")
+    Fare_label.grid(row=12, column=0)
+
+    Seats_label = Label(add_bus_app, text="Total Seats")
+    Seats_label.grid(row=13, column=0)
+
+    # Add Record to Database Button
+    add_bus_btn = Button(add_bus_app, text="Add Bus")
+    add_bus_btn.grid(row=14, column=1, pady=50)
+
+#********************* ADD BUS WINDOW **********************
+
+
+#********************* SEARCH BUS WINDOW *******************    
 # Search Bus Window
 def SearchBus():
     search_bus_app = Tk()
     search_bus_app.geometry('700x500+120+120')
     labelExample = Label(search_bus_app, text = "Welcome Customer!").pack()
-    home_btn = Button(search_bus_app, text="Home", command=search_bus_app.destroy).pack()    
+    home_btn = Button(search_bus_app, text="Home", command=search_bus_app.destroy).pack()
+
+#********************* SEARCH BUS WINDOW *******************    
+    
 
 # Buttons
 label_add = Label(root, text='(For Operators)', font=('Product Sans Regular', 15), bg="#ffd5bc")
