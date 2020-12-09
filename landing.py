7da039343de5c3c59c4d9a83c0ff1303069fe08f
Shrish Sharma <shrish0608@gmail.com>
2020-12-09 21:32:35 +0530

    Revamped UI of Add Bus and Search Bus

--- landing.py
+++ landing.py
@@ -12,6 +12,7 @@ from PIL import ImageTk,Image
 
 # Adding Custom Font
 pyglet.font.add_file('./Assets/Product Sans Regular.ttf')
+pyglet.font.add_file('./Assets/PSBold.ttf')
 
 #root layout
 root = Tk()
@@ -21,7 +22,7 @@ root.geometry("700x500+120+120")
 # this removes the maximize button
 root.resizable(0,0)
 
-# image as a background
+# image as a background for root
 load = Image.open('./Assets/landing.png')
 render = ImageTk.PhotoImage(load)
 img = Label(root, image=render)
@@ -33,11 +34,20 @@ def AddBus():
     add_bus_app = Toplevel()
     add_bus_app.geometry('700x500+120+120')
 
-    labelExample = Label(add_bus_app, text = "Welcome Operator!").grid(row=0, column=1, columnspan=10)
-    #home_btn = Button(add_bus_app, text="Home", command=add_bus_app.destroy).pack()
+    # image as a background for Add Bus
+    load_add_bg = Image.open('./Assets/add_bus_bg.png')
+    render_add_bus = ImageTk.PhotoImage(load_add_bg)
+    img_add_bus = Label(add_bus_app, image=render_add_bus)
+    img_add_bus.image = render_add_bus
+    img_add_bus.place(x=0, y=0)
+
+    labelExample = Label(add_bus_app, text = "Welcome Operator!", font=('PSBold', 18), bg='#D1456E', fg='#fff').grid(row=0, column=1, columnspan=10, pady=10)
+
+    home_button = Button(add_bus_app, image=home_btn, command=add_bus_app.destroy, bd=0, bg="#D1456E")
+    home_button.place(x=10, y=10)
 
     # Header
-    head_label = Label(add_bus_app, text="Add Details of Buses").grid(row=1, column=1, pady=10, columnspan=10)
+    head_label = Label(add_bus_app, text="Add Details of Buses", font=('PSBold', 9), bg='#D1456E', fg='#fff').grid(row=1, column=1, pady=10, columnspan=10)
 
     #Getting Inputs    
     Agency_name = Entry(add_bus_app, width=60)
@@ -74,37 +84,37 @@ def AddBus():
     Seats.grid(row=12, column=1)
 
     # Adding Entry Labels
-    Agency_name_label = Label(add_bus_app, text="Agency Name")
-    Agency_name_label.grid(row=2, column=0)
+    Agency_name_label = Label(add_bus_app, text="Agency Name", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
+    Agency_name_label.grid(row=2, column=0, padx=50)
 
-    Contact_Number_label = Label(add_bus_app, text="Contact Number")
+    Contact_Number_label = Label(add_bus_app, text="Contact Number", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Contact_Number_label.grid(row=3, column=0)
 
-    Address_label = Label(add_bus_app, text="Address")
+    Address_label = Label(add_bus_app, text="Address", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Address_label.grid(row=4, column=0)
 
-    Operator_Id_label = Label(add_bus_app, text="Operator Id")
+    Operator_Id_label = Label(add_bus_app, text="Operator Id", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Operator_Id_label.grid(row=5, column=0)
 
-    Bus_Type_label = Label(add_bus_app, text="Bus Type")
+    Bus_Type_label = Label(add_bus_app, text="Bus Type", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Bus_Type_label.grid(row=6, column=0)
 
-    loc_From_label = Label(add_bus_app, text="From Location")
+    loc_From_label = Label(add_bus_app, text="From Location", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     loc_From_label.grid(row=7, column=0)
 
-    loc_To_label = Label(add_bus_app, text="To Loaction")
+    loc_To_label = Label(add_bus_app, text="To Loaction", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     loc_To_label.grid(row=8, column=0)
 
-    Dep_Time_label = Label(add_bus_app, text="Departure Time")
+    Dep_Time_label = Label(add_bus_app, text="Departure Time", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Dep_Time_label.grid(row=9, column=0)
 
-    Arrival_Time_label = Label(add_bus_app, text="Arrival TIme")
+    Arrival_Time_label = Label(add_bus_app, text="Arrival TIme", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Arrival_Time_label.grid(row=10, column=0)
 
-    Fare_label = Label(add_bus_app, text="$ Fare")
+    Fare_label = Label(add_bus_app, text="$ Fare", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Fare_label.grid(row=11, column=0)
 
-    Seats_label = Label(add_bus_app, text="Total Seats")
+    Seats_label = Label(add_bus_app, text="Total Seats", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     Seats_label.grid(row=12, column=0)
 
     # Submit Function for Database
@@ -150,10 +160,12 @@ def AddBus():
         Fare.delete(0, END)
         Seats.delete(0, END)
 
-
     # Add Record to Database Button
-    add_bus_btn = Button(add_bus_app, text="Add Bus", command=submit)
-    add_bus_btn.grid(row=14, column=1, pady=50)
+    submit_btn = Button(add_bus_app, image=add_btn, bd=0, bg='#D1456E', command=submit).place(x=300, y=350)
+    
+    #add_bus_btn = Button(add_bus_app, text="Add Bus", command=submit)
+    #add_bus_btn.grid(row=14, column=1, pady=50)
+    
 
 #********************* ADD BUS WINDOW ENDS******************************************************
 
@@ -164,23 +176,31 @@ def SearchBus():
     search_bus_app = Toplevel()
     search_bus_app.geometry('700x500+120+120')
 
-    labelExample = Label(search_bus_app, text = "Welcome Customer!").grid(row=0, column=1)
-    home_btn = Button(search_bus_app, text="Home", command=search_bus_app.destroy).grid(row=1, column=1)
+    # image as a background for Add Bus
+    load_src_bg = Image.open('./Assets/search_bus_bg.png')
+    render_src_bus = ImageTk.PhotoImage(load_src_bg)
+    img_src_bus = Label(search_bus_app, image=render_src_bus)
+    img_src_bus.image = render_src_bus
+    img_src_bus.place(x=0, y=0)
+
+    labelExample = Label(search_bus_app, text = "Welcome Customer!", font=('PSBold', 18), bg='#D1456E', fg='#fff').grid(row=0, column=1, pady=10)
+    #home_btn = Button(search_bus_app, text="Home", command=search_bus_app.destroy).grid(row=1, column=1)
 
     # Header
-    head_label = Label(search_bus_app, text="Search Your Bus!").grid(row=2, column=1)
+    head_label = Label(search_bus_app, text="Search Your Bus!", font=('PSBold', 9), bg='#D1456E', fg='#fff').grid(row=2, column=1)
 
     # Bus Type DropDown
     click = StringVar()
     click.set("Choose Your Bus Type")
     bus_type_dropd = OptionMenu(search_bus_app, click, "AC", "Non-AC", "AC-Sleeper", "Non-AC-Sleeper", "All Types")
-    bus_type_dropd.grid(row=3, column=1, pady=20)
+    bus_type_dropd.config(width = 40)
+    bus_type_dropd.grid(row=3, column=1, pady=30)
 
     # Labels
-    loc_From_label = Label(search_bus_app, text="From Location")
-    loc_From_label.grid(row=5, column=0, padx=10)
+    loc_From_label = Label(search_bus_app, text="From Location", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
+    loc_From_label.grid(row=5, column=0, padx=50)
 
-    loc_To_label = Label(search_bus_app, text="To Loaction")
+    loc_To_label = Label(search_bus_app, text="To Loaction", font=('Product Sans Regular', 10), fg='#fff', bg='#D1456E')
     loc_To_label.grid(row=6, column=0, padx=10)
 
     # Entries
@@ -220,9 +240,7 @@ def SearchBus():
 
 
     # Search Button
-    search_btn = Button(search_bus_app, text="Search", command=Search)
-    search_btn.grid(row=7, column=1, pady=20)
-
+    search_btn = Button(search_bus_app, image=src_btn, bd=0, bg='#D1456E', command=Search).place(x=270, y=240)    
 
 #********************* SEARCH BUS WINDOW ENDS************************************************************    
     
@@ -239,6 +257,15 @@ add_bus.place(x=20, y=400)
 search_bus_img = PhotoImage(file = './Assets/search_bus.png')
 search_bus = Button(root, image=search_bus_img, bd=0, bg="#ffd5bc", activebackground="#ffd5bc", command=SearchBus)
 search_bus.place(x=470, y=400)
+
+# Add Bus App Button Image (Global)
+add_btn = ImageTk.PhotoImage(Image.open("./Assets/add_bus_child.png"))
+
+# Search Bus App Button Image (Global)
+src_btn = ImageTk.PhotoImage(Image.open("./Assets/search_bus_child.png"))
+
+# Hime Button (Global)
+home_btn = ImageTk.PhotoImage(Image.open("./Assets/home.png"))
 
 #********************************************** DATABASE *************************************************#
 
