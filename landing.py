1eea91a9798094e09472d6b8b6a5474056672152
Shrish Sharma <shrish0608@gmail.com>
2020-12-09 22:49:12 +0530

    Started Adding External DB
    Unfinished Though

--- landing.py
+++ landing.py
@@ -269,20 +269,5 @@ home_btn = ImageTk.PhotoImage(Image.open("./Assets/home.png"))
 
 #********************************************** DATABASE *************************************************#
 
-# Creating Database
-conn = sqlite3.connect('bus_list.db')
-
-# Create Cursor
-c = conn.cursor()
-
-# Creating Table
-c.execute("""CREATE TABLE if not exists BusList(Agency_Name varchar(30) not null, Contact_Number int(10) not null, Address varchar(30) not null, Operator_Id int(10) not null, Bus_Type varchar(15) not null, loc_From varchar(50) not null, loc_To varchar(50) not null, Departure_Time varchar(10) not null, Arrival_Time varchar(10) not null, fare int(10), num_seats int(50) not null, primary key(Operator_Id))""")
-
-# Commit Changes
-conn.commit()
-
-# Closing Connection
-conn.close()
-
 #********************************************** DATABASE *************************************************#
 root.mainloop()
